ed05d79f7de14f359b2ed0980dd688d7c3ed1b37
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-02-14 16:25:10 +0600

    all transaction part complete

--- js/transaction_shared.js
+++ js/transaction_shared.js
@@ -1,6 +1,5 @@
 let availableBal = parseFloat(document.getElementById("balance").innerText);
 
-
 // validation shared function
 function validation(amnt) {
   if (amnt < 1 || isNaN(amnt)) {
@@ -14,14 +13,11 @@ function validation(amnt) {
   }
 }
 
-// date genarate 
-function dateTime(){
+// date genarate
+function dateTime() {
   const date = new Date();
   return date;
 }
-
-
-
 
 // noakhali balance
 const donateNoakhali = document.getElementById("donateNoakhali");
@@ -29,7 +25,7 @@ let noaTotal = parseFloat(document.getElementById("noaTotal").innerText);
 
 donateNoakhali.addEventListener("click", function () {
   let noakhaliAmnt = document.getElementById("noakhaliAmnt").value;
-  
+
   noakhaliAmnt = validation(noakhaliAmnt);
 
   if (noakhaliAmnt !== undefined) {
@@ -40,34 +36,106 @@ donateNoakhali.addEventListener("click", function () {
     document.getElementById("balance").innerText = `${availableBal}`;
     document.getElementById("noakhaliAmnt").value = "";
 
-   
-  // History section append 
-    const timeDate=dateTime();
-  const historyDiv = document.createElement("div");
-  historyDiv.className = "border border-[#1111111A] rounded-2xl p-6 bg-white shadow-md space-y-3";
-  
-  // Creating content
-  historyDiv.innerHTML = `
+    // History section append
+    const timeDate = dateTime();
+    const historyDiv = document.createElement("div");
+    historyDiv.className =
+      "border border-[#1111111A] rounded-2xl p-6 bg-white shadow-md space-y-3";
+
+    // Creating content
+    historyDiv.innerHTML = `
     <h3 class="font-bold text-xl">${noakhaliAmnt} Taka is Donated for flood at Noakhali,Bangladesh</h3>
     
     <p class="text-[#111111B3] font-light">Date: ${timeDate}</p>
   `;
-  
-  // Append to history section
-  historySec.appendChild(historyDiv);
-    
-  
 
-
+    // Append to history section
+    historySec.appendChild(historyDiv);
   } else {
     document.getElementById("noakhaliAmnt").value = "";
     alert("Enter a valid amount");
   }
 });
 
-// feni balance 
+// feni balance
+
+const donateFeni=document.getElementById("donateFeni");
+let feniTotal=parseFloat(document.getElementById("feniTotal").innerText);
+
+donateFeni.addEventListener("click", function () {
+  let feniAmnt = document.getElementById("feniAmnt").value;
+
+  feniAmnt = validation(feniAmnt);
+
+  if (feniAmnt !== undefined) {
+    availableBal -= feniAmnt;
+    feniTotal += feniAmnt;
+    document.getElementById("feniTotal").innerText = `${feniTotal}`;
+
+    document.getElementById("balance").innerText = `${availableBal}`;
+    document.getElementById("feniAmnt").value = "";
+
+    // History section append
+    const timeDate = dateTime();
+    const historyDiv = document.createElement("div");
+    historyDiv.className =
+      "border border-[#1111111A] rounded-2xl p-6 bg-white shadow-md space-y-3";
+
+    // Creating content
+    historyDiv.innerHTML = `
+    <h3 class="font-bold text-xl">${feniAmnt} Taka is Donated for flood at Feni,Bangladesh</h3>
+    
+    <p class="text-[#111111B3] font-light">Date: ${timeDate}</p>
+  `;
+
+    // Append to history section
+    historySec.appendChild(historyDiv);
+  } else {
+    document.getElementById("feniAmnt").value = "";
+    alert("Enter a valid amount");
+  }
+});
 
 
 
+// aid for student
 
-// aid for student 
+const aidStudent=document.getElementById("aidStudent");
+let aidTotal=parseFloat(document.getElementById("aidTotal").innerText);
+
+
+aidStudent.addEventListener("click", function () {
+  let aidAmnt = document.getElementById("aidAmnt").value;
+
+  aidAmnt = validation(aidAmnt);
+
+  if (aidAmnt !== undefined) {
+    availableBal -= aidAmnt;
+    aidTotal += aidAmnt;
+    document.getElementById("aidTotal").innerText = `${aidTotal}`;
+
+    document.getElementById("balance").innerText = `${availableBal}`;
+    document.getElementById("aidAmnt").value = "";
+
+    // History section append
+    const timeDate = dateTime();
+    const historyDiv = document.createElement("div");
+    historyDiv.className =
+      "border border-[#1111111A] rounded-2xl p-6 bg-white shadow-md space-y-3";
+
+    // Creating content
+    historyDiv.innerHTML = `
+    <h3 class="font-bold text-xl">${aidAmnt} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h3>
+    
+    <p class="text-[#111111B3] font-light">Date: ${timeDate}</p>
+  `;
+
+    // Append to history section
+    historySec.appendChild(historyDiv);
+  } else {
+    document.getElementById("aidAmnt").value = "";
+    alert("Enter a valid amount");
+  }
+});
+
+

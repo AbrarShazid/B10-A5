18a1542602563899868b478a8f84451f82a98a1d
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-02-14 17:48:53 +0600

    respponsive for mobile

--- js/blog.js
+++ js/blog.js
@@ -1,12 +1,19 @@
-// home button functionallity 
+// home button functionallity
 
+const homeBtn = document.getElementById("home-button");
+homeBtn.addEventListener("click", function () {
+  window.location.href = "../index.html";
+});
+// on scroll blur effect added
+document.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  const navBar = document.getElementById("nav-Bar");
 
-const homeBtn=document.getElementById("home-button")
-homeBtn.addEventListener('click',function(){
-
-  window.location.href="../index.html";
-
-})
-
-
-
+  if (window.scrollY > 50) {
+    header.classList.add("scrolling", "bg-transparent", "backdrop-blur-xl");
+    navBar.classList.add("scrolling", "bg-transparent", "backdrop-blur-xl");
+  } else {
+    header.classList.remove("scrolling", "bg-transparent", "backdrop-blur-xl");
+    navBar.classList.remove("scrolling", "bg-transparent", "backdrop-blur-xl");
+  }
+});

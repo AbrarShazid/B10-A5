34da5a0dee1d9db0691b0a79dac41a708829d88e
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-02-14 16:03:06 +0600

    noakhali section complete

--- js/donation.js
+++ js/donation.js
@@ -1,65 +1,71 @@
-// on scroll blur effect added 
-document.addEventListener("scroll", function() {
+// on scroll blur effect added
+document.addEventListener("scroll", function () {
   const header = document.getElementById("header");
   const navBar = document.getElementById("nav-Bar");
 
   if (window.scrollY > 50) {
-    header.classList.add("scrolling","bg-transparent","backdrop-blur-md");
-    navBar.classList.add("scrolling","bg-transparent","backdrop-blur-md");
+    header.classList.add("scrolling", "bg-transparent", "backdrop-blur-md");
+    navBar.classList.add("scrolling", "bg-transparent", "backdrop-blur-md");
   } else {
-    header.classList.remove("scrolling","bg-transparent","backdrop-blur-md");
-    navBar.classList.remove("scrolling","bg-transparent","backdrop-blur-md");
+    header.classList.remove("scrolling", "bg-transparent", "backdrop-blur-md");
+    navBar.classList.remove("scrolling", "bg-transparent", "backdrop-blur-md");
   }
 });
 
+// blog button functionality
 
-// blog button functionality 
+const blogButton = document.getElementById("blog-button");
+blogButton.addEventListener("click", function () {
+  window.location.href = "Other-Page/blog.html";
+});
 
-const blogButton=document.getElementById('blog-button')
-blogButton.addEventListener('click',function(){
+// donation and history button toggle
 
-  window.location.href = "Other-Page/blog.html"; 
-
-})
-
-// donation and history button toggle 
-
-const donationBtn = document.getElementById('donation');
-const historyBtn = document.getElementById('history');
-const donationSection=document.getElementById('donation-section')
-const historySection=document.getElementById('history-section')
+const donationBtn = document.getElementById("donation");
+const historyBtn = document.getElementById("history");
+const donationSection = document.getElementById("donation-section");
+const historySec = document.getElementById("historySection");
 
 // If historyBtn is clicked
-historyBtn.addEventListener('click', function() {
+historyBtn.addEventListener("click", function () {
   donationBtn.classList.remove("bg-[#B4F461]", "text-[#111]", "border-none");
-  donationBtn.classList.add("text-[#111111B3]", "border", "border-[#1111114D]", "bg-transparent");
+  donationBtn.classList.add(
+    "text-[#111111B3]",
+    "border",
+    "border-[#1111114D]",
+    "bg-transparent"
+  );
 
-  historyBtn.classList.remove("text-[#111111B3]", "border", "border-[#1111114D]", "bg-transparent");
+  historyBtn.classList.remove(
+    "text-[#111111B3]",
+    "border",
+    "border-[#1111114D]",
+    "bg-transparent"
+  );
   historyBtn.classList.add("bg-[#B4F461]", "text-[#111]", "border-none");
 
-  donationSection.classList.add("hidden")
-  historySection.classList.remove("hidden")
-
-
-
+  donationSection.classList.add("hidden");
+  historySec.classList.remove("hidden");
 });
 
 // If donationBtn is clicked
-donationBtn.addEventListener('click', function() {
-
+donationBtn.addEventListener("click", function () {
   historyBtn.classList.remove("bg-[#B4F461]", "text-[#111]", "border-none");
-  historyBtn.classList.add("text-[#111111B3]", "border", "border-[#1111114D]", "bg-transparent");
+  historyBtn.classList.add(
+    "text-[#111111B3]",
+    "border",
+    "border-[#1111114D]",
+    "bg-transparent"
+  );
 
-  donationBtn.classList.remove("text-[#111111B3]", "border", "border-[#1111114D]", "bg-transparent");
+  donationBtn.classList.remove(
+    "text-[#111111B3]",
+    "border",
+    "border-[#1111114D]",
+    "bg-transparent"
+  );
   donationBtn.classList.add("bg-[#B4F461]", "text-[#111]", "border-none");
 
-  historySection.classList.add("hidden")
-  donationSection.classList.remove("hidden")
-
+  historySec.classList.add("hidden");
+  donationSection.classList.remove("hidden");
 });
-
-
-
-
-
-
--- js/transaction_shared.js
+++ js/transaction_shared.js
@@ -1,5 +1,6 @@
 let availableBal = parseFloat(document.getElementById("balance").innerText);
 
+
 // validation shared function
 function validation(amnt) {
   if (amnt < 1 || isNaN(amnt)) {
@@ -13,13 +14,22 @@ function validation(amnt) {
   }
 }
 
+// date genarate 
+function dateTime(){
+  const date = new Date();
+  return date;
+}
+
+
+
+
 // noakhali balance
 const donateNoakhali = document.getElementById("donateNoakhali");
 let noaTotal = parseFloat(document.getElementById("noaTotal").innerText);
 
 donateNoakhali.addEventListener("click", function () {
   let noakhaliAmnt = document.getElementById("noakhaliAmnt").value;
-
+  
   noakhaliAmnt = validation(noakhaliAmnt);
 
   if (noakhaliAmnt !== undefined) {
@@ -29,7 +39,28 @@ donateNoakhali.addEventListener("click", function () {
 
     document.getElementById("balance").innerText = `${availableBal}`;
     document.getElementById("noakhaliAmnt").value = "";
+
+   
+  // History section append 
+    const timeDate=dateTime();
+  const historyDiv = document.createElement("div");
+  historyDiv.className = "border border-[#1111111A] rounded-2xl p-6 bg-white shadow-md space-y-3";
+  
+  // Creating content
+  historyDiv.innerHTML = `
+    <h3 class="font-bold text-xl">${noakhaliAmnt} Taka is Donated for flood at Noakhali,Bangladesh</h3>
+    
+    <p class="text-[#111111B3] font-light">Date: ${timeDate}</p>
+  `;
+  
+  // Append to history section
+  historySec.appendChild(historyDiv);
+    
+  
+
+
   } else {
+    document.getElementById("noakhaliAmnt").value = "";
     alert("Enter a valid amount");
   }
 });
